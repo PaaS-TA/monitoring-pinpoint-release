18298cf35461b14eddd7e5ccb3af267050cfa315
Minsoo, Kim <32118423+thouy@users.noreply.github.com>
2022-01-07 17:12:18 +0900

    Update create.sh

--- create.sh
+++ create.sh
@@ -1,3 +1,3 @@
-bosh create-release --force --tarball paasta-pinpoint-monitoring-release.tgz --name paasta-pinpoint-monitoring-release --version 1.1
+bosh create-release --force --tarball paasta-pinpoint-monitoring-release.tgz --name paasta-pinpoint-monitoring-release --version 1.2
 
 bosh ur paasta-pinpoint-monitoring-release.tgz
