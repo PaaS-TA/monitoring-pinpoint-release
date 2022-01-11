6c4c49008fde765ddade420ba6ab4802d622dc92
YoongJwe <78844504+YoongJwe@users.noreply.github.com>
2022-01-11 10:26:12 +0900

    Update create.sh

--- create.sh
+++ create.sh
@@ -1,3 +1,18 @@
-bosh create-release --force --tarball paasta-pinpoint-monitoring-release.tgz --name paasta-pinpoint-monitoring-release --version 1.2
+#!/bin/bash
 
-bosh ur paasta-pinpoint-monitoring-release.tgz
+if [ -d ./.dev_builds ]; then
+  echo "Delete .dev_builds folder."
+  rm -rf .dev_builds
+fi
+
+if [ -d ./dev_releases ]; then
+  echo "Delete dev_releases folder."
+  rm -rf dev_releases
+fi
+
+if [ -f ./paasta-pinpoint-monitoring-release-1.2.tgz ]; then
+  echo "Delete paasta-pinpoint-monitoring-release-1.2.tgz file."
+  rm -rf paasta-pinpoint-monitoring-release-1.2.tgz
+fi
+
+bosh create-release --force --tarball paasta-pinpoint-monitoring-release-1.2.tgz --name paasta-pinpoint-monitoring-release --version 1.2
